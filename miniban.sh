07a74b7df51e0528dd20a2b3c3cead196567aa6f
Martin Clementz <martincclementz@gmail.com>
2021-11-19 10:24:38 +0100

    oppgaver

---
+++ miniban.sh
@@ -0,0 +1,19 @@
+# miniban.sh Main script - watch for system authorisation events relating to SSH (secure
+# shell), keep track of the number of failures per IP address, and when this is greater than or
+# equal to 3, ban the IP address (see next point)
+
+
+# Husk F-Lock
+
+# ------ Oppgave -------
+# Lese loggen til ssh /var/log/auth.log
+
+# ------ Oppgave -------
+# Lese loggen til ssh /var/log/auth.log
+
+# ------ Oppgave -------
+# Sjekke om ip-adressen har logget inn feil mange ganger
+
+
+# ------ Oppgave -------
+# Banne ip adressen med ./ban.sh <ip-adresse>
