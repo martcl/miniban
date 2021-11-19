c97fa3cc5ff54eb612947705b9f3b910e46067d3
Martin Clementz <martincclementz@gmail.com>
2021-11-19 10:08:26 +0100

    oppgaver

--- ban.sh
+++ ban.sh
@@ -1,8 +1,16 @@
-echo "Programmet starter"
+
+
+# ban.sh Ban an IP address using iptables and add the IP address together with a ban
+# timestamp to a persistent flat database file miniban.db (see format below)
 
 IP=$1
 
+# ----- oppgave -----
+# Sjekke at det er en gyldig ip adresse
+
+
+# ----- oppgave -----
+# Bruk iptabels til å banne ip adressen ny regel
+
+
 echo "$IP,$(date +%s)" >> miniban.db
-
-cat miniban.db
-
