c74ef3e8f695a85858c3a5e7be158bbd1442dbd8
Martin Clementz <martincclementz@gmail.com>
2021-11-23 22:02:24 +0100

    Update ban.sh

--- ban.sh
+++ ban.sh
@@ -1,5 +1,3 @@
-
-
 # ban.sh Ban an IP address using iptables and add the IP address together with a ban
 # timestamp to a persistent flat database file miniban.db (see format below)
 
@@ -9,15 +7,17 @@ IP=$1
 # Sjekker om ip adressen er gyldig
 # (Ganske sikker på at deb fungerer, men det er ikke noe poeng
 # Å bruke den når vi driver å tester uansett så la den være kommentert)
-#if (! $IP =~ ^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$); do
-#    echo "Ikke gyldig ip adresse"
-#    exit 1
+if [ ! "$IP" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]; then
 
-
+    echo "IP adress: $IP is not valid"
+    exit 1
+fi
 # ----- oppgave -----
 # Lager regel med iptabels som banner ip adresser
-iptabels -A INPUT -s "$IP" -p tcp --destination-port 22 -j DROP
+iptables -A INPUT -s "$IP" -j REJECT
+iptables-save
 
 
 # Lagere ip-adressen i databasen sammen med en timestamp
 echo "$IP,$(date +%s)" >> miniban.db
+echo "banned: $IP"
