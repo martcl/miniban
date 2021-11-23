79352742eebf5f29241e39f530d958804fbf3a82
Martin Clementz <martincclementz@gmail.com>
2021-11-23 19:01:35 +0100

    Update miniban.sh

--- miniban.sh
+++ miniban.sh
@@ -1,21 +1,49 @@
+#!/bin/bash
 # miniban.sh Main script - watch for system authorisation events relating to SSH (secure
 # shell), keep track of the number of failures per IP address, and when this is greater than or
 # equal to 3, ban the IP address (see next point)
 
+# Dette er en liste som inneholder ipadresser og hvor mange ganger de failet på å logge inn
+declare -A FAIL
 
-# Husk F-Lock
+# Loopen leser alle nye linjer i ssh loggen og setter dem inn i LINE variabelen
 
-# ------ Oppgave -------
-# Hint: Use the tail command to monitor activity in the logfile /var/log/auth.log
-# Lese loggen til ssh /var/log/auth.log
+# Loopen leser alle nye linjer i ssh loggen og setter dem inn i LINE variabelen
+tail -f -n0 /var/log/auth.log | while read LINE; do
+        # Hvis linjen inneholder substringen "Failed"
+        ## Kanksje bruke grep?
+        if [[ "$LINE" == *"Failed"* ]]; then
+                echo "$LINE"
 
-# ------ Oppgave --------
-# Sjekke om ip-adressen har logget inn feil mange ganger
+                # Henter ut IP fra linjen med regular-exspression
+                # FOrbedring?? enda bedre regex
+                IP=$(echo $LINE | grep -oP '(\d{1,3}\.){3}\d{1,3}')
 
-# ------ Oppgave --------
-# Hint: IFS="," read IP TIMESTAMP < miniban.db; echo $IP $TIMESTAMP
-# Lese loggen til ssh /var/log/auth.log
+                echo $IP
+
+                # Hvis det er første gang brukeren har failet
+                if [ ${FAIL[$IP]+_} ]; then # Hvis det finnes en Ip adresse i Arrayet
+                        # Hvis brukeren har failet 3 eller flere ganger
+                        if [ ${FAIL[$IP]} -ge 2 ]; then
+                                # Her kan vi banne ip-en
+                                # ban.sh $IP
+                                echo "du er bannet, og kan ikke logge på"
+
+                        # Hvis brukeren failer +1
+                        else
+                                FAIL[$IP]=$(( ${FAIL[$IP]} + 1 ))
+                                echo "Pluss 1, du har brukt ${FAIL[$IP]} forsøk"
+                        fi
+                else  # Hvis det ikke finnes i Arrayet. legg det til
+                        echo "Dette er en ny IP-adresse"
+                        FAIL[$IP]=1
+
+                fi
 
 
-# ------ Oppgave --------
-# Banne ip adressen med ./ban.sh <ip-adresse>
+        # Hvis brukeren suksessfult klarer å logge inn
+        elif [[ "$LINE" == *"Sucess"* ]]; then
+                # Forbedring.. Fjerene fra FAIL
+                echo "Sucess! :-)"
+        fi
+done
