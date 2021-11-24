b6cb6ef0af9f989b8fde23bb48b0531771b57a08
Thatedvardguy <74411725+Thatedvardguy@users.noreply.github.com>
2021-11-24 11:25:46 +0100

    Added flock

--- ban.sh
+++ ban.sh
@@ -1,12 +1,14 @@
 # ban.sh Ban an IP address using iptables and add the IP address together with a ban
 # timestamp to a persistent flat database file miniban.db (see format below)
-
+(
+flock -x 100
 IP=$1
 
 # ----- oppgave -----
 # Sjekker om ip adressen er gyldig
 # (Ganske sikker på at deb fungerer, men det er ikke noe poeng
 # Å bruke den når vi driver å tester uansett så la den være kommentert)
+
 if [ ! "$IP" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]; then
 
     echo "IP adress: $IP is not valid"
@@ -21,3 +23,4 @@ iptables-save
 # Lagere ip-adressen i databasen sammen med en timestamp
 echo "$IP,$(date +%s)" >> miniban.db
 echo "banned: $IP"
+) 100>lockfile.lck
