b6fb9314887ec36a4103a7a45e7b37607c1165e6
Martin Clementz <martincclementz@gmail.com>
2021-11-25 13:43:28 +0100

    Whitelist (#8)
    * Create miniban.whitelist
    
    * Update miniban.sh
    
    * Update ban.sh
    
    * Update miniban.sh
    
    Endret noen kommentarer, og fjernet gamle kommentarer som ikke var hensiktsmessige å ha med.
    
    * Update unban.sh
    
    * Update ban.sh
    
    * Update miniban.sh
    
    * Update miniban.sh
    
    Co-authored-by: eliasheimdal <94675192+eliasheimdal@users.noreply.github.com>
    Co-authored-by: perhber <94675239+perhber@users.noreply.github.com>

--- ban.sh
+++ ban.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
-# ban.sh Ban an IP address using iptables and add the IP address together with a ban
-# timestamp to a persistent flat database file miniban.db (see format below)
+# ban.sh 
+# Ban an IP address using iptables and add the IP address together with a ban
+# timestamp to a persistent flat database file miniban.db.
+
 IP=$1
 
-# ----- oppgave -----
 # Sjekker om ip adressen er gyldig
-# (Ganske sikker på at deb fungerer, men det er ikke noe poeng
-# Å bruke den når vi driver å tester uansett så la den være kommentert)
-
-if [ ! "$IP" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]; then
-
+if [[ ! "$IP" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
     echo "IP adress: $IP is not valid"
     exit 1
 fi
-# ----- oppgave -----
-# Lager regel med iptabels som banner ip adresser
 
+# Lager regel med iptabels som banner ip adresser
 iptables -A INPUT -s "$IP" -j REJECT > /dev/null
 iptables-save > /dev/null
 
-# Lagere ip-adressen i databasen sammen med en timestamp
+# Lagrer ip-adressen i databasen sammen med en timestamp
 echo "$IP,$(date +%s)" >> miniban.db
 echo "---> banned $IP"
--- miniban.sh
+++ miniban.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-# miniban.sh Main script - watch for system authorisation events relating to SSH (secure
+# miniban.sh 
+# Main script - watch for system authorisation events relating to SSH (secure
 # shell), keep track of the number of failures per IP address, and when this is greater than or
-# equal to 3, ban the IP address (see next point)
+# equal to 3, ban the IP address.
 
-# Dette er en liste som inneholder ipadresser og hvor mange ganger de failet på å logge inn
+echo "miniban is now turned ON"
+
+# Dette er en liste som inneholder ipadresser og hvor mange ganger de failet på å logge inn:
 declare -A FAIL
 
 trap func exit
@@ -13,11 +16,11 @@ sudo ./unban.sh &
 function func() {
         echo ""
 
-        ps aux | grep "unban.sh" | awk '{print $2}' | while read NUM; do
+        ps aux | grep "unban.sh" | awk '{print $2}' > /dev/null | while read NUM; do
                 kill -9 $NUM 
 
         done
-        echo "miniban is now turned OFF"
+        echo " miniban is now turned OFF"
 }
 
 # Loopen leser alle nye linjer i ssh loggen og setter dem inn i LINE variabelen
@@ -25,7 +28,6 @@ tail -f -n0 /var/log/auth.log | while read LINE; do
         # Hvis linjen inneholder substringen "Failed"
         if [[ "$LINE" == *"Failed"* ]]; then
                 # Henter ut IP fra linjen med regular-exspression
-                # FOrbedring?? enda bedre regex
                 IP=$(echo $LINE | grep -oP '(\d{1,3}\.){3}\d{1,3}')               
 
                 echo "request from $IP"
@@ -33,7 +35,7 @@ tail -f -n0 /var/log/auth.log | while read LINE; do
                 # Hvis det er første gang brukeren har failet
                 if [ ${FAIL[$IP]+_} ]; then # Hvis det finnes en Ip adresse i Arrayet
                         # Hvis brukeren har failet 3 eller flere ganger
-                        if [ ${FAIL[$IP]} -ge 2 ]; then
+                        if [ ${FAIL[$IP]} -ge 2 ] && grep -v "$IP$" miniban.whitelist; then
                                 # Her kan vi banne ip-en
                                 bash ./ban.sh $IP
                                 unset "FAIL[$IP]"
@@ -47,8 +49,6 @@ tail -f -n0 /var/log/auth.log | while read LINE; do
                 fi
         # Hvis brukeren suksessfult klarer å logge inn
         elif [[ "$LINE" == *"Accepted"* ]]; then
-                # Forbedring.. Fjerene fra FAIL
                 unset "FAIL[$IP]"
         fi
 done
-
