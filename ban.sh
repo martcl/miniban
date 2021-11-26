55f4ed6d1b41ee639588d091ae4579b0abb2280b
perhber <94675239+perhber@users.noreply.github.com>
2021-11-26 10:03:34 +0100

    Update ban.sh
    Omformulerte kommentarer til engelsk

--- ban.sh
+++ ban.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
+
 # Ban an IP address using iptables and add the IP address together with a ban
 # timestamp to a persistent flat database file miniban.db.
 
 IP=$1
 
-# Sjekker om ip adressen er gyldig
+# Check if the IP address is valid
 if [[ ! "$IP" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
     echo "IP adress: $IP is not valid"
     exit 1
 fi
 
-# Lager regel med iptabels som banner ip adresser
+# Makes a rule in iptables that ban the IP address
 iptables -A INPUT -s "$IP" -j REJECT > /dev/null
 iptables-save > /dev/null
 
-# Lagrer ip-adressen i databasen sammen med en timestamp
+# Saves the IP address and timestamp in the database
 echo "$IP,$(date +%s)" >> miniban.db
 echo "---> banned $IP"
