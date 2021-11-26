fa76c3ec28dfd9efbed1f634bb55708b0d4fe0ee
eliasheimdal <94675192+eliasheimdal@users.noreply.github.com>
2021-11-25 23:50:37 -0800

    comments and cleanup (#10)

--- unban.sh
+++ unban.sh
@@ -1,36 +1,38 @@
 #!/bin/bash
 
-# unban.sh 
 # Periodically check banned IPs in miniban.db and remove them if the ban has
 # expired (10 minutes). Remove the iptables rule for the IP address
 
+# IP input
 IP_I=$1
 
-while true; do
-        if [[ -z "$IP_I" ]]; then
-                while true; do
+# Checks if the file was given an argument
+if [[ -z "$IP_I" ]]; then
+        # If not, running loop to continuously check IP bantime
+        while true; do
                 TIME_NOW=$(date +%s)
                 sleep 1
+                
+                # Reads from miniban.db to check if an IP has excited the bantime
                 while IFS=',' read -r IP TIMESTAMP; do
                         if [ $(( $TIME_NOW - $TIMESTAMP )) -ge 10 ]; then
                                 echo "---> unban $IP"
                                 sudo iptables -D INPUT -s "$IP" -j REJECT > /dev/null
                                 sudo iptables-save > /dev/null
                                 sed -i  "/$IP,$TIMESTAMP/d" miniban.db
-
                         fi
                 done < miniban.db
-                done
+        done
         
-        elif [[ "$IP_I" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-                echo "--> unban $IP_I"
-                sudo iptables -D INPUT -s "$IP_I" -j REJECT > /dev/null
-                sudo iptables-save > /dev/null
-                sed -i  "/$IP,$TIMESTAMP/d" miniban.db
-                break
-        else
-                echo "slutt"
-                break
-        fi
-done
+# Check if the given argument is a valid IPv4 adress
+elif [[ "$IP_I" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+        echo "--> unban $IP_I"
+        sudo iptables -D INPUT -s "$IP_I" -j REJECT > /dev/null
+        sudo iptables-save > /dev/null
+        sed -i  "/$IP,$TIMESTAMP/d" miniban.db
+        
+else
+        echo "Not a valid IP address"
+fi
+
 
